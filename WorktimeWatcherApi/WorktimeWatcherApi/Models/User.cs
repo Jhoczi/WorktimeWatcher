903d36fb7464b1d17256b98062b9bb0819fa28e7
Jakub Hoczek <hoczek.jakub@gmail.com>
2022-01-23 15:56:34 +0100

    Added files to watch in git

---
+++ WorktimeWatcherApi/WorktimeWatcherApi/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace WorktimeWatcherApi.Models;
+
+public class User
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+}
